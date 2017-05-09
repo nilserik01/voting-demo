1572f8e10b77599b3b1744360a52123df092fb2a
Lab User <Lab User>
2017-05-09 21:31:02 +0000

    changing the voting options

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Dev"
-optionB = "Ops"
+optionA = "Fotboll"
+optionB = "Socker"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
